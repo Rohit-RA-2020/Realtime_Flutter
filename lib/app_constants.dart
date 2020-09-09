d346083051101921ee40032e4b5e1d111d594c70
Rohit ranjan <ranjanrohit812@gmail.com>
2020-09-09 10:25:04 +0530

    Working files

---
+++ lib/app_constants.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const Color kBackgroundColorDark = Color.fromARGB(0xff, 0x17, 0x0b, 0x0f);
+const Color kBackgroundColor = Color.fromARGB(0xff, 0x36, 0x05, 0x10);
+
+const Color kSliderColor = Colors.teal;
